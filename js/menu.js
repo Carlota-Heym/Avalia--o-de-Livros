651888c405854112bfba9e7a82d0516a2b1c75be
Carlota-Heym <heymcarlota@gmail.com>
2025-08-01 12:19:46 +0100

    adaptação ao mobile

---
+++ js/menu.js
@@ -0,0 +1,25 @@
+dicators">
+    < button type = "button" data - bs - target="#carouselExampleCaptions" data - bs - slide - to="0" class="active" aria - current="true"
+aria - label="Slide 1" ></button >
+<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+</div >
+    <div class="carousel-inner<!DOCTYPE html>
+<html lang=" en">
+
+    < head >
+    <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Avaliação de Livros </title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+                integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+                <link rel="stylesheet" href="./css/style.css">
+                </head>
+                <script>
+                    const hamburger = document.getElementById("hamburger");
+                    const menu = document.getElementById("menu");
+
+    hamburger.addEventListener("click", () => {
+                        menu.classList.toggle("active");
+    });
+                </script>
